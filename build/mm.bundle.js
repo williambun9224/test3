ca2a6e7b90992beec3b36d3347b8c3f6e3d0b6f8
williambun9224 <w.williambun@gmail.com>
2018-04-11 17:13:52 +0700

    first commit

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -71967,7 +71967,7 @@ angular.module('mm.core')
     "demo_sites": {"student": {"url": "https://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "https://school.demo.moodle.net", "username": "teacher", "password": "moodle"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "",
+    "siteurl": "http://185.201.8.24/learningonline",
     "multisitesdisplay": "select",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
